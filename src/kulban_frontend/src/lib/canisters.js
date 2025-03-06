63ee84e382a224fa4d6605c7cb59f803ce5b06ac
Eduardo <cediasfl@gmail.com>
2025-03-05 21:16:57 -0300

    Update canister URL for local development environment

--- src/kulban_frontend/src/lib/canisters.js
+++ src/kulban_frontend/src/lib/canisters.js
@@ -14,5 +14,5 @@ function dummyActor() {
 
 const buildingOrTesting =
 	building || process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development";
-export const canisterURL = buildingOrTesting ? `http://${canisterId}.raw.localhost:4943` : ``;
+export const canisterURL = buildingOrTesting ? `http://localhost:3000` : ``;
 export const backend = buildingOrTesting ? dummyActor() : createActor(canisterId);
