5479bedf0a73bd3baa9d382c333dcdf4ea3a798a
diegoxter <diegoxter1006@gmail.com>
2025-03-07 10:22:47 -0300

    forge install: forge-std
    v1.9.6

--- src/kulban_contracts/test/KanbanProject.t.sol
+++ src/kulban_contracts/test/KanbanProject.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {KanbanProject} from "../src/Deployer.sol";
 
 contract KanbanProjectTest is Test {
